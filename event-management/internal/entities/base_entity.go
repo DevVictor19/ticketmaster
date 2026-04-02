03e3778352eebf4c1c79ee42fb4a20ffef3a2d0e
DevVictor19 <antoniovictor12@live.com>
2026-04-02 20:30:52 -0300

    feat(event-management): add beforeCreate hook

--- event-management/internal/entities/base_entity.go
+++ event-management/internal/entities/base_entity.go
@@ -1,10 +1,18 @@
 package entities
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type baseEntity struct {
 	ID        uint      `json:"id"`
 	UUID      string    `json:"uuid" gorm:"uniqueIndex"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (b *baseEntity) BeforeCreate() {
+	b.UUID = uuid.New().String()
 }
