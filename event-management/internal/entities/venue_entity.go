fb3221673e5dd340ec390ce302951f65555fd1ab
DevVictor19 <antoniovictor12@live.com>
2026-04-03 11:29:09 -0300

    feat(event-management): implement scan interface for Venue

--- event-management/internal/entities/venue_entity.go
+++ event-management/internal/entities/venue_entity.go
@@ -1,8 +1,28 @@
 package entities
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+)
+
 type Venue struct {
 	BaseEntity
-	Events   []*Event        `json:"events" gorm:"foreignKey:VenueID"`
-	Location string          `json:"location" gorm:"index"`
-	SeatMap  map[string]bool `json:"seat_map" gorm:"type:json"`
+	Events   []*Event `json:"events,omitempty" gorm:"foreignKey:VenueID"`
+	Location string   `json:"location" gorm:"index"`
+	SeatMap  SeatMap  `json:"seat_map" gorm:"type:json"`
+}
+
+type SeatMap map[string]bool
+
+func (s SeatMap) Value() (driver.Value, error) {
+	return json.Marshal(s)
+}
+
+func (s *SeatMap) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("failed to scan SeatMap: %v", value)
+	}
+	return json.Unmarshal(bytes, s)
 }
