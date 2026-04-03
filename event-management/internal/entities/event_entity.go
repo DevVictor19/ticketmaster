c83a1877306c399761b94ae4893db39a433f8133
DevVictor19 <antoniovictor12@live.com>
2026-04-03 11:30:09 -0300

    feat(event-management): add omitempty for json

--- event-management/internal/entities/event_entity.go
+++ event-management/internal/entities/event_entity.go
@@ -4,10 +4,10 @@ import "time"
 
 type Event struct {
 	BaseEntity
-	Venue       *Venue       `json:"venue"`
+	Venue       *Venue       `json:"venue,omitempty"`
 	VenueID     uint         `json:"venue_id"`
-	Performers  []*Performer `json:"performers" gorm:"many2many:event_performers"`
-	Tickets     []*Ticket    `json:"tickets"`
+	Performers  []*Performer `json:"performers,omitempty" gorm:"many2many:event_performers"`
+	Tickets     []*Ticket    `json:"tickets,omitempty"`
 	Date        time.Time    `json:"date" gorm:"index"`
 	Name        string       `json:"name" gorm:"index"`
 	Description *string      `json:"description" gorm:"type:text"`
--- event-management/internal/entities/ticket_entity.go
+++ event-management/internal/entities/ticket_entity.go
@@ -3,10 +3,10 @@ package entities
 type Ticket struct {
 	BaseEntity
 	EventID uint         `json:"event_id" gorm:"index"`
-	Event   *Event       `json:"event" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Event   *Event       `json:"event,omitempty" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	Price   uint         `json:"price"`
 	Seat    string       `json:"seat" gorm:"index"`
-	Status  TicketStatus `json:"status" gorm:"index"`
+	Status  TicketStatus `json:"status,omitempty" gorm:"index"`
 }
 
 type TicketStatus string
