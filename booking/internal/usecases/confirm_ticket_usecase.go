ebb24dd214281ba4836d6d2843da88efad8eceea
DevVictor19 <antoniovictor12@live.com>
2026-04-04 20:54:08 -0300

    feat(booking): implement ConfirmTicketUseCase

---
+++ booking/internal/usecases/confirm_ticket_usecase.go
@@ -0,0 +1,48 @@
+package usecases
+
+import (
+	"context"
+	"errors"
+
+	"github.com/DevVictor19/booking/internal/entities"
+	"github.com/DevVictor19/booking/internal/repositories"
+	"github.com/DevVictor19/booking/internal/services"
+)
+
+type ConfirmTicketUseCase struct {
+	ticketRepo    repositories.TicketRepository
+	ticketLockSvc services.TicketLockService
+}
+
+func NewConfirmTicketUseCase(
+	ticketRepo repositories.TicketRepository,
+	ticketLockSvc services.TicketLockService) *ConfirmTicketUseCase {
+
+	return &ConfirmTicketUseCase{
+		ticketRepo:    ticketRepo,
+		ticketLockSvc: ticketLockSvc,
+	}
+}
+
+var ErrTicketNotReserved = errors.New("ticket is not reserved for this user")
+
+type ConfirmTicketInput struct {
+	TicketUUID        string
+	UserUUID          string
+	PaymentMethodUUID string
+}
+
+func (uc *ConfirmTicketUseCase) Execute(ctx context.Context, input ConfirmTicketInput) error {
+	isReserved, err := uc.ticketLockSvc.CheckTicketReservation(ctx, input.TicketUUID, input.UserUUID)
+	if err != nil {
+		return err
+	}
+	if !isReserved {
+		return ErrTicketNotReserved
+	}
+
+	// TODO: Implement payment integration here
+	// TOdo: publish event on Kafka to notify other services about the confirmation
+
+	return uc.ticketRepo.UpdateStatus(ctx, input.TicketUUID, entities.TicketStatusSold)
+}
