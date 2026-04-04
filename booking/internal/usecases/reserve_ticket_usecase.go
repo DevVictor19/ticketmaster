904aaaaf8ea8c3a4f4afbd30e4842790ed6012d8
DevVictor19 <antoniovictor12@live.com>
2026-04-04 17:52:52 -0300

    feat(booking): add reserve ticket uc

---
+++ booking/internal/usecases/reserve_ticket_usecase.go
@@ -0,0 +1,51 @@
+package usecases
+
+import (
+	"context"
+	"errors"
+
+	"github.com/DevVictor19/booking/internal/repositories"
+	"github.com/DevVictor19/booking/internal/services"
+)
+
+type ReserveTicketUseCase struct {
+	ticketRepo    repositories.TicketRepository
+	ticketLockSvc services.TicketLockService
+}
+
+func NewReserveTicketUseCase(
+	ticketRepo repositories.TicketRepository,
+	ticketLockSvc services.TicketLockService) *ReserveTicketUseCase {
+
+	return &ReserveTicketUseCase{
+		ticketRepo:    ticketRepo,
+		ticketLockSvc: ticketLockSvc,
+	}
+}
+
+var ErrTicketNotAvailable = errors.New("ticket not available for reservation")
+
+type ReserveTicketInput struct {
+	TicketUUID string
+	UserUUID   string
+}
+
+func (uc *ReserveTicketUseCase) Execute(ctx context.Context, input ReserveTicketInput) error {
+	isAvailable, err := uc.ticketRepo.CheckTicketAvailability(ctx, input.TicketUUID)
+	if err != nil {
+		return err
+	}
+	if !isAvailable {
+		return ErrTicketNotAvailable
+	}
+
+	err = uc.ticketLockSvc.ReserveTicket(ctx, input.TicketUUID, input.UserUUID)
+	if err != nil {
+		if errors.Is(err, services.ErrTicketAlreadyReserved) {
+			return ErrTicketNotAvailable
+		}
+		return err
+	}
+
+	return nil
+}
