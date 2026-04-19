1b32e95cf5b3d9abe725da94239a1ff489ce0ec7
DevVictor19 <antoniovictor12@live.com>
2026-04-19 11:41:26 -0300

    feat(search): cache results in search uc

--- search/internal/usecases/search_events_usecase.go
+++ search/internal/usecases/search_events_usecase.go
@@ -14,18 +14,43 @@ type SearchEventsInput struct {
 
 type SearchEventsUC struct {
 	searchEngineSvc services.SearchEngineService
+	searchCacheSvc  services.SearchCacheService
 }
 
-func NewSearchEventsUC(searchEngineSvc services.SearchEngineService) *SearchEventsUC {
+func NewSearchEventsUC(
+	searchEngineSvc services.SearchEngineService,
+	searchCacheSvc services.SearchCacheService,
+) *SearchEventsUC {
+
 	return &SearchEventsUC{
 		searchEngineSvc: searchEngineSvc,
+		searchCacheSvc:  searchCacheSvc,
 	}
 }
 
 func (uc *SearchEventsUC) Execute(ctx context.Context, input SearchEventsInput) (*services.SearchEventsResponse, error) {
-	return uc.searchEngineSvc.SearchEvents(ctx, services.SearchEventsRequest{
+	req := services.SearchEventsRequest{
 		Query: input.Query,
 		Page:  input.Page,
 		Size:  input.Size,
-	})
+	}
+
+	cached, err := uc.searchCacheSvc.Get(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	if cached != nil {
+		return cached, nil
+	}
+
+	result, err := uc.searchEngineSvc.SearchEvents(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := uc.searchCacheSvc.Set(ctx, req, result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
